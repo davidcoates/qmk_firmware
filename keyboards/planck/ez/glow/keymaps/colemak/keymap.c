454c86b40c9512bce7511bc9c1581e363595bc86
David <davidcoates95@gmail.com>
2024-09-01 17:52:24 +1000

    tweak spacing

--- keyboards/planck/ez/glow/keymaps/colemak/keymap.c
+++ keyboards/planck/ez/glow/keymaps/colemak/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOTE,       KC_BSPC,
     KC_MINUS,       KC_A,           KC_R,           KC_S,           KC_T,           KC_G,           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_SCLN,
     TD(DANCE_0),    KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLASH,       MO(_ARROW),
-    TD(DANCE_1),    TO(_GAME),      KC_LEFT_ALT,    KC_LEFT_CTRL,   OSL(_SYMBOL),   KC_SPACE,       KC_NO,          OSL(_NUMPAD),   KC_LEFT_GUI,   ST_MACRO_0,     ST_MACRO_1,     KC_ENTER
+    TD(DANCE_1),    TO(_GAME),      KC_LEFT_ALT,    KC_LEFT_CTRL,   OSL(_SYMBOL),   KC_SPACE,       KC_NO,          OSL(_NUMPAD),   KC_LEFT_GUI,    ST_MACRO_0,     ST_MACRO_1,     KC_ENTER
   ),
 
   [_SYMBOL] = LAYOUT_planck_grid(
